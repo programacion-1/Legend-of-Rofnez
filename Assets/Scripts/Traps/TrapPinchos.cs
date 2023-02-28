d7456d1b3e8159d931c6379f7e68a8785e9323e9
Sol <lalobax42@gmail.com>
2023-02-28 12:39:42 -0300

    tampas mal hechas por sol monti

---
+++ Assets/Scripts/Traps/TrapPinchos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Core;
+
+public class TrapPinchos : Trap
+{
+    private Animator Anim;
+    public override void UniqueStartSettings()
+    {
+         Anim = GetComponent<Animator>();
+         SetTriggerOn();
+    }
+    void FixedUpdate() 
+    {
+  
+    }
+    public override void TrapActivatedBehaviour()
+    {
+        Anim.SetBool("IsAtacking",true);
+        Anim.SetTrigger("AttackTriger");
+    }
+    public override void TrapDeactivatedBehaviour()
+    {
+        Anim.SetBool("IsAtacking",false);
+    }
+    public override void TrapEffect(Health target)
+    {
+        
+    }
+    public override IEnumerator waitToReactivate()
+    {
+        yield return new WaitForSeconds(GetWaitTimeTrapDesactivater());
+        TrapActivatedBehaviour();
+    }
+}
